81633f64a2bc7bf436396653f367e829d7db3780
Dulaj Kulathunga <dulajku@jkintranet.com>
2019-12-05 13:50:51 +0530

    added first files

---
+++ src/test/java/com/azure/cicd/AzureCiCdTestingProjectMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azure.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureCiCdTestingProjectMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
